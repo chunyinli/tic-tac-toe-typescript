ebad09e4e24eb6e8cd4b1188781bab3e52226100
Chunyin Li <lllchun87@gmail.com>
2021-03-10 23:51:10 -0500

    react learning tic tac toe game.

---
+++ src/components/square.jsx
@@ -0,0 +1,12 @@
+function Square(props) {
+  return (
+    <button
+      className="square"
+      onClick={props.onClick}
+    >
+      {props.value}
+    </button>
+  );
+}
+
+export default Square
---
+++ src/index.tsx
@@ -0,0 +1,8 @@
+import ReactDOM from 'react-dom';
+import Game from './components/game';
+import './index.css';
+
+ReactDOM.render(
+  <Game />,
+  document.getElementById('root')
+);
